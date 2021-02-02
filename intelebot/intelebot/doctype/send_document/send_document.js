bb51456de510958103186cf502109bb30765b93a
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-02-02 10:50:39 +0530

    Send document doctype deleted

--- intelebot/intelebot/doctype/send_document/send_document.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Aerele Technologies Private Limited and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Send Document', {
-	// refresh: function(frm) {
-
-	// }
-});
